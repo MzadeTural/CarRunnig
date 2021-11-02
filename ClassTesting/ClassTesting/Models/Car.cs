16c0403b493a1ed55a1c1548c5b08407c872b81f
Mjollnnirr <farid.mammadov@code.edu.az>
2021-11-02 20:34:42 +0400

    InitalCommit

---
+++ ClassTesting/ClassTesting/Models/Car.cs
@@ -0,0 +1,109 @@
+﻿using System;
+namespace ClassTesting.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        private double _consuption;
+        public double Consuption {
+            get
+            {
+                return _consuption;
+            }
+            private set
+            {
+                if (value > 0 && value < 300)
+                {
+                    _consuption = value;
+                }
+            }
+        }
+
+        private double _tankCapacity;
+        public double TankCapacity
+        {
+            private set
+            {
+                if (value > 0)
+                {
+                    _tankCapacity = value;
+                }
+            }
+            get
+            {
+                return _tankCapacity;
+            }
+        }
+
+        private double _currentTankCap;
+        public double CurrentTankCap
+        {
+            get
+            {
+                return _currentTankCap;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    //_currentTankCap = value;
+                    if (_tankCapacity - _currentTankCap > value)
+                    {
+                        _currentTankCap = _tankCapacity;
+                    }
+                    else
+                    {
+                        _currentTankCap = value;
+                    }
+                }
+            }
+        }
+
+
+        public Car(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public Car(double consuption, double tankCapacity)
+        {
+            Consuption = consuption;
+            TankCapacity = tankCapacity;
+            CurrentTankCap = TankCapacity;
+        }
+
+        public string Refuel(double bensin)
+        {
+            CurrentTankCap += bensin;
+            return $"Your current tank is {CurrentTankCap}";
+        }
+        public string Refuel()
+        {
+            CurrentTankCap = TankCapacity;
+            return $"Your current tank is {CurrentTankCap}";
+
+        }
+
+        public string Drive(double distance)
+        {
+            double currentCons = distance * (Consuption / 100);
+            if (currentCons < CurrentTankCap)
+            {
+                CurrentTankCap -= currentCons;
+                return $"Your car gone so far and your current gas is {CurrentTankCap}";
+            }
+            else
+            {
+                return "You cannot go this far";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Your current Tank is {CurrentTankCap}";
+        }
+    }
+}
---
+++ ClassTesting/ClassTesting/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using ClassTesting.Models;
+
+namespace ClassTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(10, 40);
+
+
+            NewPlace:
+            Console.WriteLine("Plea2se Choose\n1 for Refuel\n2 for drive\n3for info");
+            string input = Console.ReadLine().Trim().ToUpper();
+
+            switch (input)
+            {
+                case "1":
+                    car.Refuel();
+                    goto NewPlace;
+                case "2":
+                    Console.WriteLine(car.Drive(HowMuchDrive()));
+                    goto NewPlace;
+                case "3":
+                    Console.WriteLine(car.ToString());
+                    goto NewPlace;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    goto NewPlace;
+            }
+        }
+
+        public static double HowMuchDrive()
+        {
+            TryAgain:
+            Console.Write("Please enter the value that you want to drive: ");
+            string input = Console.ReadLine();
+            double convValue = 0;
+            try
+            {
+                convValue = Convert.ToDouble(input);
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid!\nTry Again!");
+                goto TryAgain;
+            }
+
+            return convValue;
+        }
+    }
+}
